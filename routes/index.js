2616982a719908815e3288594b15100978ecc21b
Peter Dremstrup <peter@dremstrup.dk>
2014-08-28 02:11:24 +0200

    Implemented whole rejseplan-api, with parameter validation - close to refactoring nirvana

--- routes/index.js
+++ routes/index.js
@@ -4,48 +4,14 @@ var http = require('http');
 var parseString = require('xml2js').parseString;
 var Promise = require('promise');
 
-var getDepartureBoard = function(req) {
-  return new Promise(function(resolve, reject) {
-    if (!req.query.id) req.query.id = 1961;
-    if (!req.query.useTog) req.query.useTog = 1;
-    if (!req.query.useMetro) req.query.useMetro = 1;
-    if (!req.query.useBus) req.query.useBus = 1;
-
-    var options = {
-      host: 'konkurrence.rejseplanen.dk',
-      path: '/bin/rest.exe/departureBoard?id=' + req.query.id + '&useTog=' + req.query.useTog + '&useMetro=' + req.query.useMetro + '&useBus=' + req.query.useBus
-    };
-
-    callback = function(response) {
-      var str = '';
-
-      //another chunk of data has been recieved, so append it to `str`
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      //the whole response has been recieved, so we just print it out here
-      response.on('end', function () {
-            parseString(str, function (err, result) {
-              if (err) reject(err);  
-              resolve(result);
-            });
-      });
-    }
-
-    http.request(options, callback).end();
-  });
+var config = {
+  host: 'xmlopen.rejseplanen.dk',
+  bin: '/bin/rest.exe'
 };
 
-var getLocation = function(req) {
+var callEndpoint = function(options) {
   return new Promise(function(resolve, reject) {
-    if (!req.query.id) req.query.id = 1961;
-
-    var options = {
-      host: 'konkurrence.rejseplanen.dk',
-      path: '/bin/rest.exe/location?input=' + req.query.input
-    };
-
+    console.log(options);
     callback = function(response) {
       var str = '';
 
@@ -58,15 +24,7 @@ var getLocation = function(req) {
       response.on('end', function () {
         parseString(str, function (err, result) {
           if (err) reject(err);
-          var length = result.LocationList.StopLocation.length;
-          var i = 0;
-          result.LocationList.StopLocation.forEach(function(stop, index) {
-            getDepartureBoard({'query': {'id': stop.$.id} }).then(function(busses) {
-              stop.$.departureBoard = busses;
-            });
-            i++;
-          });
-          if (i == length) resolve(result);              
+          resolve(result);              
         });
       });
     }
@@ -75,17 +33,119 @@ var getLocation = function(req) {
   });
 };
 
+var transportType = function(req) {
+  var transportString = '';
+  if (req.query.useTog) transportString = '&useTog=' + req.query.useTog;
+  if (req.query.useBus) transportString += '&useBus=' + req.query.useBus;
+  if (req.query.useMetro) transportString += '&useMetro=' + req.query.useMetro;
+  
+  return transportString;
+};
+
+var time = function(req) {
+  var timeString = '';
+  if (req.query.date) timeString += '&date=' + req.query.date;
+  if (req.query.time) timeString += '&time=' + req.query.time;
+  if (req.query.searchForArrival) timeString += req.query.searchForArrival;
+  
+  return timeString;
+};
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/journeyDetail', function(req, res) {
+  var options = {
+    host: config.host,
+    path: config.bin + '/journeyDetail?ref=' + req.query.ref
+  };
+  
+  callEndpoint(options).then(function(result) { res.json(result); });
+});
+
+router.get('/stopsNearby', function(req, res) {
+  var options = {
+    host: config.host,
+    path: config.bin + '/stopsNearby?coordX=' + req.query.coordX + '&coordY=' + req.query.coordY + '&maxRadius=' + req.query.maxRadius + '&maxNumber=' + req.query.maxNumber
+  };
+  
+  callEndpoint(options).then(function(result) { res.json(result); });
+});
+
 router.get('/departureBoard', function(req, res) {
-  getDepartureBoard(req).then(function(result) { res.json(result); });
+  var options = {
+    host: config.host,
+    path: config.bin + '/departureBoard?id=' + req.query.id + transportType(req) + time(req)
+  };
+  
+  callEndpoint(options).then(function(result) { res.json(result); });
+});
+
+router.get('/multiDepartureBoard', function(req, res) {
+  var idString = '?id1=' + req.query.id1;
+  if (req.query.id2) idString += '&id2=' + req.query.id2;
+  if (req.query.id3) idString += '&id3=' + req.query.id3;
+  if (req.query.id4) idString += '&id4=' + req.query.id4;
+  if (req.query.id5) idString += '&id5=' + req.query.id5;
+  if (req.query.id6) idString += '&id6=' + req.query.id6;
+  if (req.query.id7) idString += '&id7=' + req.query.id7;
+  if (req.query.id8) idString += '&id8=' + req.query.id8;
+  if (req.query.id9) idString += '&id9=' + req.query.id9;
+  if (req.query.id10) idString += '&id10=' + req.query.id10;
+  
+  var options = {
+    host: config.host,
+    path: config.bin + '/departureBoard' + idString + transportType(req) + time(req)
+  };
+  
+  callEndpoint(options).then(function(result) { res.json(result); });
+});
+
+router.get('/trip', function(req, res) {
+  if (req.query.originId && req.query.destId) {
+    var options = {
+      host: 'xmlopen.rejseplanen.dk',
+      path: config.bin + '/trip?originId=' + req.query.originId + '&destId=' + req.query.destId + transportType(req) + time(req)
+    };
+
+    callEndpoint(options).then(function(result) { res.json(result); });
+  }
+  else if (req.query.originId && !req.query.destId) {
+    var options = {
+      host: 'xmlopen.rejseplanen.dk',
+      path: config.bin + '/trip?originId=' + req.query.originId + '&destCoordX=' + req.query.destCoordX + '&destCoordY=' + req.query.destCoordY + '&destCoordName=' + req.query.destCoordName + transportType(req) + time(req)
+    };
+
+    callEndpoint(options).then(function(result) { res.json(result); });
+  }
+  else if (!req.query.originId && req.query.destId) {
+    var options = {
+      host: config.host,
+      path: config.bin + '/trip?originCoordX=' + req.query.originCoordX + '&originCoordY=' + req.query.originCoordY + '&originCoordName=' + req.query.originCoordName + '&destId=' + req.query.destId + transportType(req) + time(req)
+    };
+
+    callEndpoint(options).then(function(result) { res.json(result); });
+  }
+  else {
+    var options = {
+      host: config.host,
+      path: config.bin + '/trip?originCoordX=' + req.query.originCoordX + '&originCoordY=' + req.query.originCoordY + '&originCoordName=' + req.query.originCoordName + '&destCoordX=' + req.query.destCoordX + '&destCoordY=' + req.query.destCoordY + '&destCoordName=' + req.query.destCoordName + transportType(req) + time(req)
+    };
+
+    callEndpoint(options).then(function(result) { res.json(result); });
+  }
 });
 
 router.get('/location', function(req, res) {
-  getLocation(req).then(function(result) { res.json(result); });
+  if (!req.query.input) req.query.input = '';
+  var options = {
+    host: config.host,
+    path: config.bin + '/location?input=' + req.query.input + transportType(req)
+  };  
+  
+  callEndpoint(options).then(function(result) { res.json(result); });
 });
 
 module.exports = router;
